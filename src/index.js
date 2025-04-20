b0ac67d77b96c28dd9b74066a7c954ebdc2b1b4a
gildhelm <150284955+gildhelm@users.noreply.github.com>
2025-04-20 12:19:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import App from './App.js';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-window.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('ar-viewer');
-  if (root) {
-    root.innerHTML = App(); // If App is a function returning HTML
-  }
-});
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
